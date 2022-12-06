70f6d7acffdef3cb68cf3db9e7f449578bd77f08
Qaamuus Academy <112928466+qaamuusplatform@users.noreply.github.com>
2022-12-06 11:31:05 +0300

    Update admin.py

--- a_system/admin.py
+++ a_system/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from . models import *
 from  api.models import VoteModel 
-# Register your models here.
+# Register your models heree.
 admin.site.register(OurInterFriends)
 admin.site.register(AboutQaamuusInfo)
 admin.site.register(VoteModel)
