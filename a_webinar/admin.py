82bef3759715ab0b12670eccf1c6e2ff9a4b4e66
Qaamuus Academy <112928466+qaamuusplatform@users.noreply.github.com>
2022-12-13 19:17:00 +0300

    Update admin.py

--- a_webinar/admin.py
+++ a_webinar/admin.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from .models import *
-# Register your models here.
+# Register your models hee.
 
 admin.site.register(EventView)
 # admin.site.register(EventEnrolled)
