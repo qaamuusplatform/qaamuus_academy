22ecd973d1662b7a59b66d0eb80534bca8471bac
Qaamuus Academy <112928466+qaamuusplatform@users.noreply.github.com>
2022-12-16 10:38:33 +0300

    Update admin.py
    desc

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from . models import *
-# Register your admin sites modal.
+# Register your admin sites modals.
 admin.site.register(UserTypes)
 # admin.site.register(UserProfile)
 admin.site.register(CouponCode)
